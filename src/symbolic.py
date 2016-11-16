a5689ebf2be89bab3899e775b18d076853356760
Kaho Sato <kahosato93@gmail.com>
2016-11-16 11:37:24 +0000

    LexiconGenerator recognises neutral/both for priorpolarity field. Fix re-use of generator in symbolic.py.

--- src/symbolic.py
+++ src/symbolic.py
@@ -5,6 +5,17 @@ from tokeniser import Tokeniser
 
 
 class LexiconGenerator(object):
+    sentiment_score = {
+        "positive": 1,
+        "neutral": 0,
+        "negative": -1,
+        "both": 0
+    }
+    sentiment_weight = {
+        "weaksubj": 0.5,
+        "strongsubj": 1
+    }
+
     def __init__(self):
         pass
 
@@ -24,29 +35,29 @@ class LexiconGenerator(object):
         elements = line.split(" ")
         values = map(lambda elem: elem.split("=")[1], elements)
         key = values[2]
-        return (key, LexiconEntry(values[0] == "weaksubj", int(values[1]), key, values[3], values[4] == "y",
-                                  values[5] == "positive"))
+        return (key, LexiconEntry(LexiconGenerator.sentiment_weight[values[0]], int(values[1]), key, values[3],
+                                  values[4] == "y",
+                                  LexiconGenerator.sentiment_score[values[5]]))
 
 
 class LexiconEntry(object):
-    def __init__(self, is_weak, length, word, pos1, is_stemmed, is_positive):
+    def __init__(self, weight, length, word, pos1, is_stemmed, sentiment_score):
         super(LexiconEntry, self).__init__()
-        self.is_weak = is_weak
-        self.weight = is_weak and 0.5 or 1
+        self.weight = weight
         self.length = length
         self.word = word
         self.pos1 = pos1
         self.is_stemmed = is_stemmed
-        self.is_positive = is_positive
+        self.sentiment_score = sentiment_score
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return self.is_weak == other.is_weak \
+            return self.weight == other.weight \
                    and self.length == other.length \
                    and self.word == other.word \
                    and self.pos1 == other.pos1 \
                    and self.is_stemmed == other.is_stemmed \
-                   and self.is_positive == other.is_positive
+                   and self.sentiment_score == other.sentiment_score
 
 
 class SymbolicScore(object):
@@ -59,10 +70,7 @@ class SymbolicScore(object):
         for token in tokens:
             try:
                 entry = lexicon[token.value]
-                if entry.is_positive:
-                    score += 1
-                else:
-                    score -= 1
+                score += entry.sentiment_score
             except KeyError:
                 continue
         return score
@@ -73,10 +81,7 @@ class SymbolicScore(object):
         for token in tokens:
             try:
                 entry = lexicon[token.value]
-                if entry.is_positive:
-                    score += entry.weight
-                else:
-                    score -= entry.weight
+                score += entry.sentiment_score * entry.weight
             except KeyError:
                 continue
         return score
@@ -91,23 +96,27 @@ if __name__ == "__main__":
     bin_correct = 0
     wei_correct = 0
     for pos_file in pos_files:
-        tokens = Tokeniser.tokenise(pos_file)
+        # files not too long + use twice, so compute list
+        tokens = list(Tokeniser.tokenise(pos_file))
         bin_score = SymbolicScore.compute_binary(tokens, lex)
         wei_score = SymbolicScore.compute_weighted(tokens, lex)
         if bin_score >= 0: bin_correct += 1
         if wei_score >= 0: wei_correct += 1
         # 0 : positive
         # bin: positive, wei: negative
-        if bin_score >= 0 > wei_score: bin_win += 1
+        if bin_score >= 0 > wei_score:
+            bin_win += 1
         # bin: positive, wei: negative
-        elif bin_score < 0 <= wei_score: wei_win += 1
+        elif bin_score < 0 <= wei_score:
+            wei_win += 1
         else:
             bin_win += 0.5
             wei_win += 0.5
     neg_path = os.path.abspath("../data/NEG")
     neg_files = [os.path.join(neg_path, f) for f in os.listdir(neg_path)]
     for neg_file in neg_files:
-        tokens = Tokeniser.tokenise(neg_file)
+        # files not too long + use twice, so compute list
+        tokens = list(Tokeniser.tokenise(neg_file))
         bin_score = SymbolicScore.compute_binary(tokens, lex)
         wei_score = SymbolicScore.compute_weighted(tokens, lex)
         if bin_score < 0: bin_correct += 1
--- src/test_lexiconGenerator.py
+++ src/test_lexiconGenerator.py
@@ -8,8 +8,11 @@ class TestLexiconGenerator(TestCase):
     def test_generate(self):
         lex = LexiconGenerator.generate(os.path.abspath("../resources/sent_lexicon"))
         # 0: type=weaksubj len=1 word1=abandoned pos1=adj stemmed1=n priorpolarity=negative
-        assert lex["abandoned"] == LexiconEntry(True, 1, "abandoned", "adj", False, False)
+        assert lex["abandoned"] == LexiconEntry(0.5, 1, "abandoned", "adj", False, -1)
         # 32: type=strongsubj len=1 word1=abomination pos1=noun stemmed1=n priorpolarity=negative
-        assert lex["abomination"] == LexiconEntry(False, 1, "abomination", "noun", False, False)
+        assert lex["abomination"] == LexiconEntry(1, 1, "abomination", "noun", False, -1)
         # 240 type=strongsubj len=1 word1=agreeable pos1=anypos stemmed1=y priorpolarity=positive
-        assert lex["agreeable"] == LexiconEntry(False, 1, "agreeable", "anypos", True, True)
+        assert lex["agreeable"] == LexiconEntry(1, 1, "agreeable", "anypos", True, 1)
+        # type=strongsubj len=1 word1=covet pos1=verb stemmed1=y priorpolarity=both
+        assert lex["covet"] == LexiconEntry(1, 1, "covet", "verb", True, 0)
+
