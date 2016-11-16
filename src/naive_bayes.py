fd8681eccfcb45c9430ed5b5ec707330e20f06a3
Kaho Sato <kahosato93@gmail.com>
2016-11-16 15:25:40 +0000

    Implement naive bayes, untested

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -1,57 +1,92 @@
+import os
+
 from tokeniser import Tokeniser
 
 
 class NaiveBayes:
-    def __init__(self, all_docs):
+    __smooth_constant = 1
+
+    def __init__(self, training_docs, smooth=1):
+        """all_docs: all_docs[class_index] = array of paths to a document classified as class_index"""
+        self.__smooth_constant = smooth
         # p(c) -> count of documents classified as c / all docs
-        # p(f|c) -> count of f in document c / total words in the document classified as c
-        # index of each word - list
-        count_docs = map(len, all_docs)
-        total_class = len(all_docs)
-        total_docs = sum(count_docs)
-        p_c = map(lambda x: x / total_docs, count_docs)
-        # array of array of counts
-        # count_words[token] = frequency in each class
-        count_words = {}
-        # total words per class
-        total_tokens = [0] * total_class
-        for i in xrange(0, total_class):
-            for file in all_docs[i]:
+        # p(f|c) ->
+        # count of f in document c + smooth
+        # / total tokens in the document classified as c + (vocab in class c + unseen_vocab) * smooth
+        # As we keep vocabulary for each class separate, have an array of dictionaries
+        # vocabs[class][word] = frequency of word in the class
+        count_docs_per_class = map(len, training_docs)
+        classes_count = len(training_docs)
+        total_docs = sum(count_docs_per_class)
+        p_c = map(lambda x: x / total_docs, count_docs_per_class)
+
+        # total tokens per class
+        total_tokens = [0] * classes_count
+
+        # Array of vocabularies for each class
+        vocabs = [{}] * classes_count
+
+        # Array of vocabulary size for each class
+        vocab_sizes = [0] * classes_count
+
+        # populate
+        # total_tokens[i] - increment for each token
+        # vocabs[i][token] - 1 if unseen, increment if seen
+        # vocab_sizes - increment for each unseen token
+        for i in xrange(0, classes_count):
+            vocab = vocabs[i]
+            for file in training_docs[i]:
                 tokens = Tokeniser.tokenise(file)
+                freq_so_far = 0
                 for token in tokens:
                     try:
-                        freq = count_words[token]
+                        freq_so_far = vocab[token]
                     except KeyError:
-                        freq = [0] * total_class
-                        count_words[token] = freq
-                    freq[i] += 1
+                        vocab_sizes[i] += 1
+                    vocab[token] = freq_so_far + 1
                     total_tokens[i] += 1
-        for count_word in count_words:
-            for i in xrange(0, total_class):
-                count_word[i] /= total_tokens[i]
-        self.count_docs = count_docs
-        self.total_class = total_class
+
         self.total_tokens = total_tokens
-        self.vocab_size = len(count_words)
+        self.vocabs = vocabs
+        self.vocab_sizes = vocab_sizes
+        self.classes_count = len(training_docs)
         self.p_c = p_c
-        self.p_f_c = count_words
 
     def classify(self, file):
-        tokens = Tokeniser.tokenise(file)
+        tokens = list(Tokeniser.tokenise(file))
         best_prob = 0
         best_class = 0
-        for i in xrange(0, self.total_class):
-            prob = 1
+        # token -> frequency in file
+        vocabs_in_file = {}
+        # how many unseen
+        unseen_vocabs_count = [0] * self.classes_count
+        # count unseen words
+        for token in tokens:
+            # if we have seen this before in this document, increment frequency
+            try:
+                vocabs_in_file[token] += 1
+            # if we haven't, then
+            # - initialise its frequency in this document to 1
+            # - check if it was seen in a document of class i. If not then increment unseen_vocabs_count[i]
+            except KeyError:
+                vocabs_in_file[token] = 1
+                for i in xrange(0, self.classes_count):
+                    if token not in self.vocabs[i]:
+                        unseen_vocabs_count[i] += 1
+        for i in xrange(0, self.classes_count):
+            prob = self.p_c[i]
+            w = self.vocab_sizes[i] + unseen_vocabs_count[i]
             for token in tokens:
                 try:
-                    prob *= self.p_f_c[token][i]
+                    freq_in_c = self.vocabs[token]
                 except KeyError:
-                    # todo, smoothing
-                    pass
-            prob *= self.p_c[i]
+                    freq_in_c = 0
+                # p(f|c) ->
+                # count of f in document c + smooth
+                # / total tokens in the document classified as c + (vocab in class c + unseen_vocab) * smooth
+                prob *= (freq_in_c + self.__smooth_constant) \
+                        / float(self.total_tokens[i] + self.__smooth_constant * w)
             if prob > best_prob:
                 best_class = i
                 best_prob = prob
         return best_class
-        # p(c) -> count of documents classified as c / all docs
-        # p(f|c) -> count of f in document c / total words in the document classified as c
