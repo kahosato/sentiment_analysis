dc5ca006a33bbad36581037e7f940ac84046037a
Kaho Sato <kahosato93@gmail.com>
2016-12-17 19:45:53 +0000

    flip_func refactored, there should be some small bug, but ah well...

--- src/symbolic_neg.py
+++ src/symbolic_neg.py
@@ -16,26 +16,70 @@ def compute_negation_list():
             list.append(word.rstrip())
     return list
 
+#
+# def flip_punc(tokens, lexicon, neg_words, bin=True, stemmed=False):
+#     score = 0
+#     negated = False
+#     neg_array = []
+#     sentence = []
+#     for token in tokens:
+#         sentence.append(token)
+#         if isinstance(token, PunctuationToken):
+#             neg_array.append(False)
+#             if token == PunctuationToken("."):
+#                 score += compute_score_sentence(sentence, neg_array, lexicon, bin, stemmed)
+#                 sentence = []
+#                 neg_array = []
+#             negated = False
+#         elif token.value in neg_words:
+#             neg_array.append(False)
+#             negated = not negated
+#         else:
+#             neg_array.append(negated)
+#     return score
+
 
 def flip_punc(tokens, lexicon, neg_words, bin=True, stemmed=False):
-    score = 0
+    return compute_score_document(tokens, compute_neg_punc(tokens, neg_words), lexicon, bin, stemmed)
+
+
+def compute_neg_punc(tokens, neg_words):
     negated = False
     neg_array = []
-    sentence = []
     for token in tokens:
-        sentence.append(token)
         if isinstance(token, PunctuationToken):
             neg_array.append(False)
-            if token == PunctuationToken("."):
-                score += compute_score_sentence(sentence, neg_array, lexicon, bin, stemmed)
-                sentence = []
-                neg_array = []
             negated = False
         elif token.value in neg_words:
             neg_array.append(False)
             negated = not negated
         else:
             neg_array.append(negated)
+    return neg_array
+
+
+def compute_score_document(tokens, neg_array, lexicon, bin=True, stemmed=False):
+    assert len(tokens) == len(neg_array)
+    score = 0
+    for i in xrange(0, len(tokens)):
+        token = tokens[i]
+        neg_weight = neg_array[i] and -1 or 1
+        try:
+            entry = lexicon[token.value]
+            if bin:
+                score += entry.sentiment_score * neg_weight
+            else:
+                score += entry.sentiment_score * entry.weight * neg_weight
+        except KeyError:
+            if stemmed:
+                try:
+                    entry = lexicon[stem(token.value)]
+                    if bin:
+                        score += entry.sentiment_score * neg_weight
+                    else:
+                        score += entry.sentiment_score * entry.weight * neg_weight
+                except KeyError:
+                    continue
     return score
 
 
@@ -123,11 +167,12 @@ def compute_score_sentence(tokens, neg_array, lexicon, bin, stemmed):
 scope_dict = {'punc': flip_punc, 'prev': flip_prev, 'noneg': SymbolicScore.compute, 'x': flip_after_x}
 
 
-def compute_score_document(tokens, lex, method, neg_words):
+def apply_method(tokens, lex, method, neg_words):
     if method[0] == "noneg":
         return scope_dict[method[0]](tokens, lex, bin=method[1] == "b", stemmed=method[2] == "s")
     if method[0] == "x":
-        return scope_dict[method[0]](tokens, lex, neg_words, bin=method[1] == "b", stemmed=method[2] == "s", scope_size=int(method[3]))
+        return scope_dict[method[0]](tokens, lex, neg_words, bin=method[1] == "b", stemmed=method[2] == "s",
+                                     scope_size=int(method[3]))
     else:
         return scope_dict[method[0]](tokens, lex, neg_words, bin=method[1] == "b", stemmed=method[2] == "s")
 
@@ -158,8 +203,8 @@ def run_experiment(method1_disc, method2_disc, pos_files, neg_files, lex):
     wei_correct = 0
     for tokens in pos_files:
         # files not too long + use twice, so compute list
-        bin_score = compute_score_document(tokens, lex, method1, neg_words)
-        wei_score = compute_score_document(tokens, lex, method2, neg_words)
+        bin_score = apply_method(tokens, lex, method1, neg_words)
+        wei_score = apply_method(tokens, lex, method2, neg_words)
         if bin_score >= 0: bin_correct += 1
         if wei_score >= 0: wei_correct += 1
         # 0 : positive
@@ -174,8 +219,9 @@ def run_experiment(method1_disc, method2_disc, pos_files, neg_files, lex):
             wei_win += 0.5
     for tokens in neg_files:
         # files not too long + use twice, so compute list
-        bin_score = compute_score_document(tokens, lex, method1, neg_words)
-        wei_score = compute_score_document(tokens, lex, method2, neg_words)
+        bin_score = apply_method(tokens, lex, method1, neg_words)
+        wei_score = apply_method(tokens, lex, method2, neg_words)
+
         if bin_score < 0: bin_correct += 1
         if wei_score < 0: wei_correct += 1
         # bin: positive, wei: negative
